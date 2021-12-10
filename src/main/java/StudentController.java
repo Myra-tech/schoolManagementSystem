2c403d772b439350a314a0a92d2deb301b7cd4b1
Myra-tech <ance.l.tetere@gmail.com>
2021-12-10 21:06:11 +0200

    Search student function added

--- src/main/java/StudentController.java
+++ src/main/java/StudentController.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentController {
 
     static Scanner scanner = new Scanner(System.in);
+    private static ArrayList<Students> studentlist = new ArrayList<Students>();
 
     public static void addStudent() {
         System.out.print("Enter a name of the student: ");
@@ -21,5 +23,19 @@ public class StudentController {
 
         System.out.print("Enter the name of the course: "); // here you can create a drop-down menu
         String courseEnrolled = scanner.nextLine();
+    }
+
+    private static void searchStudent(int id) {
+        boolean search = true;
+        for (Students st : studentlist) {
+            if (st.getId() == id) {
+                System.out.println("Student Id: " + st.getId() + " Student Name: " + st.getName()
+                        + " Age: " + st.getAge() + " Course enrolled: " + st.getCourseEnrolled());
+                search = false;
+            }
+        }
+        if (search) {
+            System.out.println(id + " does not exist");
+        }
     }
 }
--- src/main/java/Students.java
+++ src/main/java/Students.java
@@ -28,6 +28,9 @@ public class Students {
             this.id = id;
         }
 
+    public String getName() {
+        return name;
+    }
         public void setName(String name) {
             if (!(name.isEmpty())) {
                 this.name = name;
